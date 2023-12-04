352241db58c0bedd831bd7048f40419682ff1bb2
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2023-12-03 21:35:38 -0500

    Add footer skeleton

--- ycwl-ui/src/app/app.component.ts
+++ ycwl-ui/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'ycwl-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
