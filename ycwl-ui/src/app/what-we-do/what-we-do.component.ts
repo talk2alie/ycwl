b8abb0a8420331ef3647da8c48655c1a26cafed9
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2023-12-11 20:58:48 -0500

    Add carousel

--- ycwl-ui/src/app/what-we-do/what-we-do.component.ts
+++ ycwl-ui/src/app/what-we-do/what-we-do.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../shared/banner/banner.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ycwl-what-we-do',
   standalone: true,
-  imports: [BannerComponent],
+  imports: [BannerComponent, NgbModule],
   templateUrl: './what-we-do.component.html',
   styleUrl: './what-we-do.component.css'
 })
